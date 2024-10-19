9fdaf99016c64822bb41e6379a3a442c8dfbfabe
Eduardo Carmo <eduardocarmo72@gmail.com>
2024-10-19 11:58:38 -0300

    criação do component TodoForm e sua estilização

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,6 +1,4 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-
 const Todo = ({ todo }) => {
   return (
     <div className="todo">
@@ -9,8 +7,8 @@ const Todo = ({ todo }) => {
             <p>({todo.category})</p>
         </div>
         <div>
-            <button>Completar</button>
-            <button>X</button>
+            <button className="complete">Completar</button>
+            <button className="remove">X</button>
         </div>
     </div>
   );
