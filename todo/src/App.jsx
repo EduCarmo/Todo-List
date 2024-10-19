b4be1f94e0fab19e92d01d15bd858e5f585dc08f
Eduardo Carmo <eduardocarmo72@gmail.com>
2024-10-19 15:01:36 -0300

    criação do components Search

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Todo from "./components/Todo"
 import "./App.css";
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
 
 
 function App() {
@@ -29,6 +30,8 @@ function App() {
       },
     ]
   );
+
+  const [search, setSearch] = useState("");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -66,9 +69,21 @@ function App() {
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search 
+      search={search}
+      setSearch={setSearch}
+      />
       <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
+        {todos
+        .filter((todo) => 
+        todo.text.toLowerCase().includes(search.toLowerCase()))
+        .map((todo) => (
+          <Todo 
+          key={todo.id} 
+          todo={todo} 
+          removeTodo={removeTodo} 
+          completeTodo={completeTodo}
+          />
         ))}
       </div>
       <TodoForm addTodo={addTodo}/>
